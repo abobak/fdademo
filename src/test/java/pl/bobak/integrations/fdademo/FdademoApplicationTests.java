79af4d84db22034f86f6d08b6b8c7266c112880c
Andrzej Bobak <a.bobak@interia.pl>
2023-01-17 16:28:37 +0100

    Initial project setup

---
+++ src/test/java/pl/bobak/integrations/fdademo/FdademoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bobak.integrations.fdademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
