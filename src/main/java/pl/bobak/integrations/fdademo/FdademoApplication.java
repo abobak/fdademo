4067332ad51898f6c218f85b7815dc09250fdc5f
Andrzej Bobak <a.bobak@interia.pl>
2023-01-18 16:24:26 +0100

    Remove unnecessary annotation

--- src/main/java/pl/bobak/integrations/fdademo/FdademoApplication.java
+++ src/main/java/pl/bobak/integrations/fdademo/FdademoApplication.java
@@ -2,10 +2,8 @@ package pl.bobak.integrations.fdademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class FdademoApplication {
 
 	public static void main(String[] args) {
